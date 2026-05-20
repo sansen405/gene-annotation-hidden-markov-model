562773ddb5497407db557b99eb46dd1877988ef3
sansen405 <sanjay.senthil405@gmail.com>
2026-05-19 22:54:14 -0700

    Transition Header Implementation

--- src/parsers/FNA_Parser.hpp
+++ src/parsers/FNA_Parser.hpp
@@ -9,7 +9,7 @@ namespace gene_hmm {
 
     using namespace std;
 
-    struct ChromosomeRange {
+    struct Chromosome_Range {
         string name;
         size_t start; //inclusive
         size_t end; //exclusive
@@ -20,7 +20,7 @@ namespace gene_hmm {
             static vector<Nucleotide> parse_sequence(const string& fasta_path);
             static size_t get_sequence_length(const string& fasta_path);
             static unordered_map<string, size_t> get_chromosome_offsets(const string& fasta_path);
-            static vector<ChromosomeRange> get_chromosome_ranges(const string& fasta_path);
+            static vector<Chromosome_Range> get_chromosome_ranges(const string& fasta_path);
     };
 
 }
